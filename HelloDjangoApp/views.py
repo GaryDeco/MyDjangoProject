64f13b7bb42e21c2e763261b125ac61aff7b58c5
Gary Deco <gsdecosmo@yahoo.com>
2019-05-30 19:45:07 -0400

    added html render to app

--- HelloDjangoApp/views.py
+++ HelloDjangoApp/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from datetime import datetime
 
+#def index(request):
+#    return HttpResponse("Hello, Django!")
+
+# this function renders the html response 
 def index(request):
-    return HttpResponse("Hello, Django!")
+    now = datetime.now()
+
+    html_content = "<html><head><title>Hello, Django</title></head><body>"
+    html_content += "<strong>My HTML text!</strong> on " + now.strftime("%A, %d %B, %Y at %X")
+    html_content += "</body></html>"
+
+    return HttpResponse(html_content)
